2f8d07d3ce00fbc4e9752f51877fc94226bc4472
MugishBeldar <mugishbeldar333@gmail.com>
2024-02-01 12:56:32 +0530

    chat section ui updated and backgrounnd updated

--- src/components/my-chats/myChats.tsx
+++ src/components/my-chats/myChats.tsx
@@ -5,11 +5,13 @@ import { useSelector } from "react-redux";
 import Cookies from "js-cookie";
 import { Avatar, Button } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
+import SendIcon from '@mui/icons-material/Send';
 
 const MyChats = () => {
   const [chatss, setChats] = React.useState<chatType[]>([]);
   const chats = useSelector((state: any) => state.chatState.chats);
-  // console.log("🚀 ~ MyChats ~ chats:", chats);
+  const [clickedUser, setClickedUser] = React.useState<userTypes | null>(null);
+  console.log("🚀 ~ MyChats ~ clickedUser:", clickedUser);
   const { fetchChats } = useMyChatsContrller({ setChats });
   const userInfoStringify: string | undefined = Cookies.get("USER_INFO");
   const loggedUser: userType =
@@ -19,6 +21,15 @@ const MyChats = () => {
     fetchChats();
   }, []);
 
+    React.useEffect(() => {
+      const firstChat: chatType = chats && chats[0];
+      let user;
+      if (firstChat && firstChat.users) {
+        user = firstChat.users.find((user) => user._id !== loggedUser.ID);
+      }
+      if(user)  setClickedUser(user)
+  }, [chats]);
+
   const getSender = (loggedUser: userType, users: userTypes[]) => {
     const senderName =
       users[0]?._id === loggedUser?.ID ? users[1].name : users[0].name;
@@ -26,17 +37,30 @@ const MyChats = () => {
   };
 
   const getProfilePicture = (loggedUser: userType, users: userTypes[]) => {
-    const selectedUser:any = users.filter(user => user._id !== loggedUser.ID)
-    return selectedUser[0].profilePic ? selectedUser[0].profilePic :  `https://ui-avatars.com/api/?background=random&name=${selectedUser[0].name}`;
-  }
+    const selectedUser: userTypes[] = users.filter(
+      (user) => user._id !== loggedUser.ID
+    );
+    return selectedUser[0].profilePic
+      ? selectedUser[0].profilePic
+      : `https://ui-avatars.com/api/?background=random&name=${selectedUser[0].name}`;
+  };
+
+  const getSelectedUser = (loggedUser: userType, users: userTypes[]) => {
+    // console.log(loggedUser, "loogedUser");
+    // console.log('chating user,', users);
+    const selectedUser: userTypes[] = users.filter(
+      (user) => user._id !== loggedUser.ID
+    );
+    setClickedUser(selectedUser[0]);
+  };
 
   return (
     <div className="flex h-auto my-3 border-2 rounded-lg">
       <div className="w-[30%] p-4 border-2 rounded-xl">
-        <div className="flex bg-white p-2 mb-2 border-2 rounded-lg">
+        <div className="flex bg-white px-2 py-4 mb-2 border-2 rounded-lg">
           <p className="text-2xl ml-3 w-[65%] text-gray-500">My Chats</p>
           <Button
-            sx={{ backgroundColor: "blue", fontWeight: "bold" }}
+            sx={{ backgroundColor: "Blue", fontWeight: "bold" }}
             variant="contained"
             size="small"
           >
@@ -47,29 +71,78 @@ const MyChats = () => {
         <div className="bg-white h-[79vh] border-2 rounded-lg ">
           <ul className="overflow-scroll h-full no-scrollbar">
             {chats &&
-              chats.map((chat: chatType) => (
-                <li key={chat._id} className="h-[6vh] flex items-center mx-2 px-2 hover:bg-blue-100 hover:rounded-lg">
-                  {chat.users && (
-                    <Avatar
-                      alt="Remy Sharp"
-                      src={getProfilePicture(loggedUser, chat.users)}
-                      sx={{ marginRight: "20px" }}
-                    />
-                  )}
-                  {!chat.isGroupChat && chat.users
-                    ? getSender(loggedUser, chat.users)
-                    : chat.chatName}
-                </li>
-              ))}
+              chats.map(
+                (chat: chatType) =>
+                  chat &&
+                  chat.users && (
+                    <li
+                      onClick={() =>
+                        chat.users
+                          ? getSelectedUser(loggedUser, chat?.users)
+                          : null
+                      }
+                      key={chat._id}
+                      className="h-[6vh] flex items-center mx-2 px-2 hover:bg-blue-100 hover:rounded-lg"
+                    >
+                      {chat.users && (
+                        <Avatar
+                          alt="Remy Sharp"
+                          src={getProfilePicture(loggedUser, chat.users)}
+                          sx={{ marginRight: "20px" }}
+                        />
+                      )}
+                      {!chat.isGroupChat && chat.users
+                        ? getSender(loggedUser, chat.users)
+                        : chat.chatName}
+                    </li>
+                  )
+              )}
           </ul>
         </div>
       </div>
-      <div className="w-[70%] h-[63%]p-4 border-2 rounded-xl">full chat section</div>
+      <div className="w-[70%] border-2 rounded-xl bg-white my-4 mr-4 flex flex-col">
+        {clickedUser && (
+          <div className="flex bg-white p-2 mb-2 border-b-2 rounded-lg">
+            <div className="text-2xl ml-3 w-[65%] text-gray-500">
+              {clickedUser && (
+                <div className="flex items-center">
+                  <Avatar
+                    alt="Remy Sharp"
+                    src={
+                      clickedUser.profilePic
+                        ? clickedUser.profilePic
+                        : `https://ui-avatars.com/api/?background=random&name=${clickedUser.name}`
+                    }
+                    sx={{ marginRight: "20px", width: "50px", height: "50px" }}
+                  />
+                  <p>
+                    {clickedUser.name.charAt(0).toUpperCase() +
+                      clickedUser.name.slice(1)}
+                  </p>
+                </div>
+              )}
+            </div>
+          </div>
+        )}
+
+        <div className="flex-1">{/* Messages go here */}
+        <h1>messages</h1>
+        </div>
+
+        <div className="flex items-center border-t-2">
+          <input
+            type="text"
+            className="flex-1 inline m-2 p-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500  rounded-md sm:text-sm focus:ring-1"
+            placeholder="Type your message..."
+          />
+          
+          <button className="px-4 py-2 rounded-lg bg-blue-500 text-white hover:bg-blue-700">😀 Emoji</button>
+         
+          <button className="mx-2 px-4 py-2 rounded-lg bg-green-500 text-white hover:bg-green-700"><SendIcon fontSize="small" className="mr-1 flex items-center mt-[-2px ]"/>Send</button>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default MyChats;
-
-// const userInfoStringify: string | undefined = Cookies.get("USER_INFO");
-// const loggedUser: userType = userInfoStringify && JSON.parse(userInfoStringify);
