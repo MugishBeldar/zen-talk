103ef97a3663fd6f86ba5302f6112a3f928efb52
MugishBeldar <mugishbeldar333@gmail.com>
2024-02-08 11:12:15 +0530

    chat model updated

--- new-server/model/Chat/Chat.js
+++ new-server/model/Chat/Chat.js
@@ -4,13 +4,11 @@ const moment = require("moment-timezone");
 // Create schema
 const ChatSchema = new mongoose.Schema({
   chatName: { type: String, trim: true },
-  isGroupChat: { type: Boolean, default: false },
   users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   latestMessage: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Message",
   },
-  groupAdmin: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   createdAt: {
     type: String,
     default: moment.tz("Asia/Calcutta").format("dddd DD-MM-YYYY hh:mm:ss A "),
