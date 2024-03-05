6d51f261577fb866a38ad5ec27364d512d45d725
MugishBeldar <mugishbeldar333@gmail.com>
2024-03-05 11:11:14 +0530

    fix socket.io backend side

--- new-server/server.js
+++ new-server/server.js
@@ -41,7 +41,7 @@ const createdServer = app.listen(
 );
 
 const corsOptions = {
-  origin: "http://localhost:3000", // Allow requests from this origin
+  origin: "http://localhost:3001", // Allow requests from this origin
   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"], // Allow all methods
 };
 
