9a71c5d4d40feeff3949b3ed0c02b17b2243cda5
MugishBeldar <mugishbeldar333@gmail.com>
2024-10-29 13:40:59 +0530

    Kafka server added for message stream

---
+++ kafka-server/handlers/message-stream-consumer.js
@@ -0,0 +1,41 @@
+module.exports = function createMessageStreamConsumer({ kafka, ObjectId }) {
+  return function messageStreamConsumer() {
+    const consumer = kafka.consumer({ groupId: "chat-group" });
+    consumer.connect();
+    consumer.subscribe({ topic: "add-chat-lable", fromBeginning: false });
+
+    consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        try {
+          // Parse the incoming message
+          const newMessage = JSON.parse(message.value.toString());
+
+          console.log("[+] msg : ", newMessage);
+
+          // Use insertOne to create the message
+          const insertedMsg = await global.dbConnection
+            .collection("messages")
+            .insertOne(newMessage);
+
+          // Retrieve the newly created message
+          let newLyInsertedMessage = await global.dbConnection
+            .collection("messages")
+            .findOne({ _id: insertedMsg.insertedId });
+
+          // Update the 'chats' collection with the latest message reference
+          await global.dbConnection
+            .collection("chats")
+            .updateOne(
+              { _id: new ObjectId(newMessage.chat) },
+              { $set: { latestMessage: newLyInsertedMessage._id } }
+            );
+
+          console.log("[+] msg inserted in db: ", newLyInsertedMessage);
+        } catch (error) {
+          console.error("[-] Error processing message: ", error);
+          // Additional error handling (e.g., logging, retry logic) can be added here.
+        }
+      },
+    });
+  };
+};
