31278e69d0571e3c79660b237a583b02a01d3c13
MugishBeldar <mugishbeldar333@gmail.com>
2024-11-27 11:04:57 +0530

    Fix typo

--- client/src/app/auth/login/login.tsx
+++ client/src/app/auth/login/login.tsx
@@ -111,7 +111,7 @@ export default function Login() {
               navigate("/signup");
             }}
           >
-            Singup
+            Signup
           </span>
         </p>
       </div>
